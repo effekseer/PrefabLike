73d63ff5733960632926dd1173b95f303913736c
durswd <swda.durl@gmail.com>
2021-06-13 16:17:03 +0900

    Update(WIP)

--- PrefabLike/Program.cs
+++ PrefabLike/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PrefabLike
 {
@@ -8,59 +9,153 @@ namespace PrefabLike
 		{
 			Console.WriteLine("Hello World!");
 		}
+
+		class TestNode : Node
+		{
+			public TestStruct V1;
+		}
+
+		struct TestStruct
+		{
+			public float A;
+			public float B;
+			public float C;
+		}
 	}
 
-	class TestNode : Node
+	class PrefabSyatem
 	{
-		public TestStruct V1;
+		public byte[] SavePrefab(Node node)
+		{
+			// TODO
+			return null;
+		}
+
+		public Node LoadPrefab(byte[] data)
+		{
+			// TODO
+			return null;
+		}
+
+		public Node MakePrefab(Node node)
+		{
+			// TODO
+			return null;
+		}
+
+		public Node CreateNodeFromPrefab(Node prefab)
+		{
+			// TODO
+			return null;
+		}
 	}
 
-	struct TestStruct
+	/// <summary>
+	/// Store file information.
+	/// </summary>
+	class FileInformation
 	{
-		public float A;
-		public float B;
-		public float C;
+		public string Path;
 	}
 
-	class Package : Resource
+	class Node
 	{
-		public Node RootNode;
-	}
+		// エディタ専用周りは分離したほうがいいかもしれない
 
-	class Node	
-	{
-		public Package Package;
+		/// <summary>
+		/// Editor only
+		/// </summary>
+		public FileInformation FileInfo;
+
+		/// <summary>
+		/// Editor only
+		/// </summary>
 		public Modified Modified;
 
-		// なんか色々
-	}
-
-	class Resource
-	{ 
-		// なんか色々
-		// ファイルパスやなんらかで一意に定まることを前提
+		/// <summary>
+		/// Editor only
+		/// </summary>
+		public Node Template;
 	}
 
 	/// <summary>
 	/// A class to contain differences
 	/// </summary>
 	class Modified
-	{ 
-	
+	{
+		public Dictionary<string, object> Difference = new Dictionary<string, object>();
+	}
+
+	/// <summary>
+	/// 差分をとるためのやつ
+	/// </summary>
+	class State
+	{
+		Dictionary<string, object> values = new Dictionary<string, object>();
+		public void Store(object o)
+		{
+			// 木構造未対応
+			var fields = o.GetType().GetFields();
+			foreach (var field in fields)
+			{
+				// 構造体とクラスで分岐すべき
+				var value = field.GetValue(o);
+				values.Add(field.Name, value);
+			}
+		}
+
+		public Dictionary<string, object> GenerateDifference(State state)
+		{
+			Dictionary<string, object> ret = new Dictionary<string, object>();
+
+			foreach (var value in state.values)
+			{
+				var newVal = values[value.Key];
+				if (newVal != value.Value)
+				{
+					ret.Add(value.Key, newVal);
+				}
+			}
+
+			return ret;
+		}
 	}
 
 	class CommandManager
 	{
-		public static void StartEdit(object o)
-		{ 
+		public void Undo()
+		{
+
 		}
 
-		public static void NotifyEdit(object o)
-		{ 
-		
-		}
-		public static void EndEdit(object o)
+		public void Redo()
 		{
+
+		}
+
+		public void StartEdit(object o)
+		{
+			if (o is Node)
+			{
+				// おそらくオブジェクトの種類でUndo分岐しないと辛い
+				// ステートの差分を取得して、Modiedを生成し、ModifiedもUNDOすることになる
+			}
+		}
+
+		public void NotifyEdit(object o)
+		{
+			if (o is Node)
+			{
+				// おそらくオブジェクトの種類でUndo分岐しないと辛い
+			}
+		}
+
+		public void EndEdit(object o)
+		{
+			if (o is Node)
+			{
+				// おそらくオブジェクトの種類でUndo分岐しないと辛い
+			}
 		}
 	};
 }
