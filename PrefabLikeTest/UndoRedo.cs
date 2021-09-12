cf3a59d805b962125ffde9ee6e75a6bfbb8e7f4e
durswd <swda.durl@gmail.com>
2021-09-12 14:45:05 +0900

    Undoのテスト改善

--- PrefabLikeTest/UndoRedo.cs
+++ PrefabLikeTest/UndoRedo.cs
@@ -30,18 +30,15 @@ namespace PrefabLikeTest
 		[Test]
 		public void EditField()
 		{
+			var random = new System.Random();
 			var commandManager = new CommandManager();
 			var node = new TestNodePrimitive();
 
-			node.Value1 = 2;
-			node.Value2 = 3.0f;
-			node.Value3 = "Test";
+			var assignedUnedit = Helper.AssignRandomField(random, ref node);
 
 			commandManager.StartEditFields(node);
 
-			node.Value1 = 3;
-			node.Value2 = 4.0f;
-			node.Value3 = "Test2";
+			var assignedEdit = Helper.AssignRandomField(random, ref node);
 
 			commandManager.NotifyEditFields(node);
 
@@ -49,11 +46,52 @@ namespace PrefabLikeTest
 
 			commandManager.Undo();
 
-			Assert.AreEqual(node.Value1, 2);
+			Helper.AreEqual(assignedUnedit, ref node);
 
 			commandManager.Redo();
 
-			Assert.AreEqual(node.Value1, 3);
+			Helper.AreEqual(assignedEdit, ref node);
+		}
+
+		class Helper
+		{
+			public static void AreEqual<T>(Dictionary<System.Reflection.FieldInfo, object> states, ref T o)
+			{
+				foreach(var kv in states)
+				{
+					var value = kv.Key.GetValue(o);
+					Assert.AreEqual(value, kv.Value);
+				}
+			}
+
+			public static Dictionary<System.Reflection.FieldInfo, object> AssignRandomField<T>(System.Random random, ref T o)
+			{
+				var assigned = new Dictionary<System.Reflection.FieldInfo, object>();
+
+				foreach (var field in o.GetType().GetFields())
+				{
+					if (field.FieldType == typeof(int))
+					{
+						int v = random.Next();
+						field.SetValue(o, v);
+						assigned.Add(field, v);
+					}
+					else if (field.FieldType == typeof(float))
+					{
+						float v = (float)random.NextDouble();
+						field.SetValue(o, v);
+						assigned.Add(field, v);
+					}
+					else if (field.FieldType == typeof(string))
+					{
+						string v = System.Guid.NewGuid().ToString();
+						field.SetValue(o, v);
+						assigned.Add(field, v);
+					}
+				}
+
+				return assigned;
+			}
 		}
 	}
 }
